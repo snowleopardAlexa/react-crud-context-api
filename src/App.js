cf27696ea487544757db03c8b2db66c2c9f420a0
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-27 18:24:52 -0500

    change book

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Listarticle from './components/Listarticle';
-import Createarticle from './components/Createarticle';
-import Editarticle from './components/Editarticle';
-import Detailarticle from './components/Detailarticle';
+import ListBook from './components/ListBook';
+import CreateBook from './components/CreateBook';
+import EditBook from './components/EditBook';
+import DetailBook from './components/DetailBook';
 
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
     <GlobalProvider>
-       <header className="text-center"><h5>React CRUD with Hooks and Context API</h5></header>
+       <header className="text-center"><h5>Library CRUD App</h5></header>
        <Switch>
-        <Route path="/" component={Listarticle} exact/>
-        <Route path="/create" component={Createarticle} exact/>
-        <Route path="/edit/:id" component={Editarticle} exact/>
-        <Route path="/detail/:id" component={Detailarticle} exact/>
+        <Route path="/" component={ListBook} exact/>
+        <Route path="/create" component={CreateBook} exact/>
+        <Route path="/edit/:id" component={EditBook} exact/>
+        <Route path="/detail/:id" component={DetailBook} exact/>
       </Switch>
     </GlobalProvider>
   );
