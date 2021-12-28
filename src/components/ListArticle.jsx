9aec5efdfba92bf780a8074803e4c9688e3c0d68
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-28 15:49:06 -0500

    add onchange event to form

--- src/components/ListArticle.jsx
+++ src/components/ListArticle.jsx
@@ -15,19 +15,41 @@ const [author, setAuthor] = useState('');
             <form>
                 <div className="form-group">
                     <label htmlFor="formGroupExampleInput">Article Heading</label>
-                    <input type="text" className="form-control" id="heading" />
+                    <input type="text" 
+                      className="form-control" 
+                      id="heading" 
+                      onChange={(e) => setHeading(e.target.value)}
+                      value={heading}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="formGroupExampleInput">Article Heading</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="2" />
+                    <textarea 
+                      className="form-control" 
+                      id="exampleFormControlTextarea1" 
+                      rows="2" 
+                      onChange={(e) => setsubHeading(e.target.value)} 
+                      value={subheading}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="formGroupExampleInput">Article Heading</label>
-                    <input type="text" className="form-control" id="heading" />
+                    <input 
+                     type="text" 
+                     className="form-control" 
+                     id="heading" 
+                     onChange={(e) => setDescription(e.target.value)}
+                     value={description}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="formGroupExampleInput">Article Heading</label>
-                    <input type="text" className="form-control" id="heading" />
+                    <input 
+                     type="text" 
+                     className="form-control" 
+                     id="heading" 
+                     onChange={(e) => setAuthor(e.target.value)}
+                    />
                 </div>
                 <button type="submit" className="btn btn-primary">Save Article</button>
                 <button type="button" className="btn">Cancel</button>
