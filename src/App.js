74b2db73a693277fd3e86649e9ad73cfc3d9c61d
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-31 14:45:33 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import ListArticle from './components/ListArticle';
+import CreateArticle from './components/CreateArticle';
+import EditArticle from './components/EditArticle';
+import DeleteArticle from './components/DeleteArticle';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <header className="text-center">React CRUD App</header>
+      <Switch>
+        <Route path="/" component={ListArticle} exact />
+        <Route path="/create" component={CreateArticle} exact />
+        <Route path="/edit/:id" component={EditArticle} exact />
+        <Route path="/detail/:id" component={DeleteArticle} exact />
+      </Switch>
+    </main>
   );
 }
 
